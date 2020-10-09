00dbafd07448c4834c4d4133a2bb6c92a64888bc
Tim Winfred <twinfred@gmail.com>
2020-10-08 22:40:03 -0700

    Ran multiple promises at the same time

--- LearningPromises.js
+++ LearningPromises.js
@@ -34,3 +34,37 @@ didUserDownloadVideo().then((message) => {
 }).catch((message) => {
     console.log(message);
 })
+
+// 3
+
+const recordVideoOne = new Promise((resolve, reject) => {
+    resolve('Video 1 Recorded!');
+});
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+    resolve('Video 2 Recorded!');
+});
+
+const recordVideoThree = new Promise((resolve, reject) => {
+    resolve('Video 3 Recorded!');
+});
+
+Promise.all([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree,
+]).then((messages) => {
+    messages.forEach(function (message) {
+        console.log(message);
+    })
+});
+
+// 4
+
+Promise.race([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree,
+]).then((message) => {
+    console.log(message);
+});
