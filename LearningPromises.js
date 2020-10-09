918f897aa9adb060a16abbf213c9a25acc126693
Tim Winfred <twinfred@gmail.com>
2020-10-08 22:31:39 -0700

    Setup a second promise using a return statement

--- LearningPromises.js
+++ LearningPromises.js
@@ -1,5 +1,7 @@
+// 1
+
 let p = new Promise((resolve, reject) => {
-    let a = 1 + 1;
+    let a = 1 + 2;
     if (a === 2) {
         resolve('Success');
     } else {
@@ -12,3 +14,23 @@ p.then((message) => {
 }).catch((message) => {
     console.log('This Promise was a ' + message);
 });
+
+// 2
+
+let videoDownloaded = false;
+
+function didUserDownloadVideo() {
+    return new Promise((resolve, reject) => {
+        if (!videoDownloaded) {
+            reject('Oh no! The user did not download the video');
+        } else {
+            resolve('Yay! The user did download the video');
+        }
+    })
+}
+
+didUserDownloadVideo().then((message) => {
+    console.log(message);
+}).catch((message) => {
+    console.log(message);
+})
