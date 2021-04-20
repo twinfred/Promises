8ef71609aeb808cbf2112c9afa83fc3fe1e18cd8
Tim Winfred <twinfred@gmail.com>
2021-04-20 00:25:23 -0700

    Tested myself on the 4 different Promise challenges previously learned

--- LearningPromises.js
+++ LearningPromises.js
@@ -1,70 +1,178 @@
-// 1
+/**
+  Challenge #1
+*/
 
 let p = new Promise((resolve, reject) => {
-    let a = 1 + 2;
-    if (a === 2) {
-        resolve('Success');
-    } else {
-        reject('Failure');
-    }
+  let a = 0 + 2;
+  if (a === 2) {
+    resolve({
+      message: 'Success',
+    });
+  } else {
+    reject({
+      message: 'Failure',
+    });
+  }
 });
 
-p.then((message) => {
-    console.log('This Promise was a ' + message);
-}).catch((message) => {
-    console.log('This Promise was a ' + message);
-});
-
-// 2
-
-let videoDownloaded = false;
-
-function didUserDownloadVideo() {
-    return new Promise((resolve, reject) => {
-        if (!videoDownloaded) {
-            reject('Oh no! The user did not download the video');
-        } else {
-            resolve('Yay! The user did download the video');
-        }
-    })
-}
-
-didUserDownloadVideo().then((message) => {
-    console.log(message);
-}).catch((message) => {
-    console.log(message);
+p.then((data) => {
+  console.log(data.message);
+}).catch((data) => {
+  console.log(data.message);
 })
 
-// 3
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 2;
+//     if (a === 2) {
+//         resolve('Success');
+//     } else {
+//         reject('Failure');
+//     }
+// });
 
-const recordVideoOne = new Promise((resolve, reject) => {
-    resolve('Video 1 Recorded!');
+// p.then((message) => {
+//     console.log('This Promise was a ' + message);
+// }).catch((message) => {
+//     console.log('This Promise was a ' + message);
+// });
+
+
+/**
+  Challenge #2
+*/
+
+const users = ['tim123', 'ruben456', 'darnish789', 'woong000'];
+
+function doesUserHaveAccount(username) {
+  return new Promise((resolve, reject) => {
+    if (users.indexOf(username) > -1) {
+      resolve(username + ' does have an account');
+    } else {
+      reject(username + ' does not have an account');
+    }
+  });
+}
+
+doesUserHaveAccount('isadora246').then((response) =>{
+  console.log(response);
+}).catch((response) => {
+  console.log(response);
 });
 
-const recordVideoTwo = new Promise((resolve, reject) => {
-    resolve('Video 2 Recorded!');
+doesUserHaveAccount('woong000').then((response) =>{
+  console.log(response);
+}).catch((response) => {
+  console.log(response);
 });
 
-const recordVideoThree = new Promise((resolve, reject) => {
-    resolve('Video 3 Recorded!');
-});
+// let videoDownloaded = false;
+
+// function didUserDownloadVideo() {
+//     return new Promise((resolve, reject) => {
+//         if (!videoDownloaded) {
+//             reject('Oh no! The user did not download the video');
+//         } else {
+//             resolve('Yay! The user did download the video');
+//         }
+//     })
+// }
+
+// didUserDownloadVideo().then((message) => {
+//     console.log(message);
+// }).catch((message) => {
+//     console.log(message);
+// })
+
+
+/**
+  Challenge #3
+*/
+
+const carousels = {
+  homepage: {
+    items: [1, 2, 3],
+  },
+  giftCard: {
+    items: [4, 5, 6],
+  }
+};
+
+function getCarouselData(carouselName) {
+  return new Promise((resolve, reject) => {
+    if (carousels[carouselName]) {
+      resolve(carousels[carouselName])
+    } else {
+      reject('Error: The ' + carouselName + ' carousel doesn\'t exist.')
+    }
+  })
+}
+
+const movies = {
+  popular: ['a', 'b', 'c', 'd'],
+  newReleases: ['e', 'f', 'g', 'h'],
+};
+
+function getMoviesByCategory(movieCategory) {
+  return new Promise((resolve, reject) => {
+    if (movies[movieCategory]) {
+      resolve(movies[movieCategory]);
+    } else {
+      reject('Error: The ' + movieCategory + ' category doesn\'t exist.')
+    }
+  })
+}
 
 Promise.all([
-    recordVideoOne,
-    recordVideoTwo,
-    recordVideoThree,
-]).then((messages) => {
-    messages.forEach(function (message) {
-        console.log(message);
-    })
-});
+  getCarouselData('homepage'),
+  getMoviesByCategory('popular'),
+]).then((responses) => {
+  responses.forEach(function(response) {
+    console.log(response);
+  })
+}).catch((response) => {
+  console.log(response);
+})
 
-// 4
+// const recordVideoOne = new Promise((resolve, reject) => {
+//     resolve('Video 1 Recorded!');
+// });
+
+// const recordVideoTwo = new Promise((resolve, reject) => {
+//     resolve('Video 2 Recorded!');
+// });
+
+// const recordVideoThree = new Promise((resolve, reject) => {
+//     resolve('Video 3 Recorded!');
+// });
+
+// Promise.all([
+//     recordVideoOne,
+//     recordVideoTwo,
+//     recordVideoThree,
+// ]).then((messages) => {
+//     messages.forEach(function (message) {
+//         console.log(message);
+//     })
+// });
+
+
+/**
+  Challenge #4
+*/
 
 Promise.race([
-    recordVideoOne,
-    recordVideoTwo,
-    recordVideoThree,
-]).then((message) => {
-    console.log(message);
-});
+  getCarouselData('giftCard'),
+  getMoviesByCategory('newReleases'),
+]).then((reponse) => {
+  console.log(reponse);
+}).catch((response) => {
+  console.log(response);
+})
+
+// Promise.race([
+//     recordVideoOne,
+//     recordVideoTwo,
+//     recordVideoThree,
+// ]).then((message) => {
+//     console.log(message);
+// });
